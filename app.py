e619be7b906aef86828e7ff6d01c7283a8cbeabe
JoMab <mabongojoel@gmail.com>
2024-06-26 14:44:11 +0200

    Update

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import os
 app = Flask(__name__)
 
 
-APIKEY = ZGEwODEyMmYwZW1zaGViMjU5YWJlOGZhMTUxZHAxYjkzN2Zqc243YTExMGYzZDA4OWU=
-APIHOST = currency-conversion-and-exchange-rates.p.rapidapi.com
+APIKEY = "ZGEwODEyMmYwZW1zaGViMjU5YWJlOGZhMTUxZHAxYjkzN2Zqc243YTExMGYzZDA4OWU="
+APIHOST = "currency-conversion-and-exchange-rates.p.rapidapi.com"
 
 url = "https://currency-conversion-and-exchange-rates.p.rapidapi.com/convert"
 headers = {
